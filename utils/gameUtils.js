26cf8156046b54eb1ab42d61c3eda7031448598e
Janninadg <jannina.delosgodos@pucp.edu.pe>
2024-06-25 23:43:06 -0500

    premios gameUtils

--- utils/gameUtils.js
+++ utils/gameUtils.js
@@ -3,21 +3,21 @@ const setPresentsReward = (character) => {
         switch (character) {
             case 0:
               //Swordman
-              return [14011, 14012, 14013, 14014, 14015, 14016];
+              return {i:[14011, 14012, 14013, 14014, 14015, 14016],m:'un Set de Swordman'};
             case 1:
               //Archer
-              return [14017, 14018, 14019, 14020, 14021, 14022];
+              return {i:[14017, 14018, 14019, 14020, 14021, 14022],m:'un Set de Archer'};;
             case 2:
               //Black
-              return [14023, 14024, 14025, 14026, 14027, 14028];
+              return {i:[14023, 14024, 14025, 14026, 14027, 14028],m:'un Set de Black'};;
             case 3:
               //Mage
-              return [14029, 14030, 14031, 14032, 14033, 14034];
+              return {i:[14029, 14030, 14031, 14032, 14033, 14034],m:'un Set de Mage'};;
             case 4:
               //Ninja
-              return [14035, 14036, 14037, 14038, 14039, 14040];
+              return {i:[14035, 14036, 14037, 14038, 14039, 14040],m:'un Set de Ninja'};;
             default:
-              break;
+              return null;
           }
   
     } catch (error) {
