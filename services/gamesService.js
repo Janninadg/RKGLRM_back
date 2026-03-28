92ad70d7ca4e72323dc12d00d0398379d70e13d0
Janninadg <jannina.delosgodos@pucp.edu.pe>
2026-03-27 23:42:54 -0500

    cometn

--- services/gamesService.js
+++ services/gamesService.js
@@ -265,18 +265,18 @@ class GamesService {
                         // 🎯 CASO 1: Usuario ya tiene 8004 → bloquear
                         if (userAlreadyHas8004) {
 
-                            console.log(allPrizes)
+                            // console.log(allPrizes)
                             const blockedProb = Number(allPrizes[targetIndex].probability);
                             allPrizes[targetIndex].probability = 0;
 
-                            console.log(allPrizes)
+                            // console.log(allPrizes)
 
                             const totalRemaining = allPrizes.reduce((sum, p, i) => {
                                 if (i !== targetIndex) return sum + Number(p.probability);
                                 return sum;
                             }, 0);
 
-                            console.log(totalRemaining)
+                            // console.log(totalRemaining)
 
                             allPrizes.forEach((p, i) => {
                                 if (i !== targetIndex) {
@@ -285,7 +285,7 @@ class GamesService {
                                 }
                             });
 
-                            console.log(allPrizes)
+                            // console.log(allPrizes)
 
 
                         }
